747165bb96a263ad5fab3c2fcd4f48a2ecd67a99
bncastle <bncastle@pixelbytestudios.com>
2022-08-05 14:45:58 -0400

    d

--- project/Main.gd
+++ project/Main.gd
@@ -1,16 +1,16 @@
 extends Node2D
 
-export(PackedScene) var bug
+export(Array, PackedScene) var entities:Array
 export(Resource) var info
 
 onready var version = $Control/MC/VBox/Version
 
 func _ready():
 	randomize()
-	VisualServer.set_default_clear_color(Color.peachpuff)
+	VisualServer.set_default_clear_color(Color.darkslategray)
 	
-	for i in range(20):
-		var b = bug.instance()
+	for i in range(15):
+		var b = entities[rand_range(0, entities.size())].instance()
 		b.velocity = rand_range(35.0, 67.0)
 		$Path2D.add_child(b)
 		
--- project/Pather.gd
+++ project/Pather.gd
@@ -1,11 +1,22 @@
 extends PathFollow2D
 
 export (float) var velocity = 35.0
+var prevPos : Vector2
+
+
 func _ready():
 	unit_offset = rand_range(0, 1)
-	modulate = Color(rand_range(0, 1), rand_range(0, 1), rand_range(0, 1))
+	var c = rand_range(.5, 1)
+	modulate = Color(c,c,c)
 	scale = Vector2.ONE * rand_range(0.75, 1.5)
+	prevPos = position
 
 func _process(delta):
 	offset += velocity * delta
+	
+	var dir :Vector2 = (position - prevPos).normalized()
+	
+	$AnimatedSprite.flip_h = dir.x < 0
+	
+	prevPos = position
 	
